7895ac78509b3155a592af7633c056d6ce03aec2
vimalp19 <vphb9@umsl.edu>
2016-12-02 11:00:37 -0600

    Update createAssignment.js

--- LitFlip/js/createAssignment.js
+++ LitFlip/js/createAssignment.js
@@ -88,9 +88,9 @@ jQuery(function($) {
     $(document).on ('click', '#prereading', function() {
         
 		//Used to generate the id for the question, its answer, and the delete button
-        var question = "prereadingQuestion" + questionCount;
-        var newAnswer = "answer" + questionCount;
-		var deletequestion = "deletQuestion" + questionCount;
+        var question = "prereadingQuestion " + questionCount;
+        var newAnswer = "answer " + questionCount;
+		var deletequestion = "deletQuestion " + questionCount;
         
 		//Proceed with displaying the options in pre-reading question and an input box for its answer, and the delete question button
         var prereadingQuestion = $('<br><br> <div class="form-group addQuestion"> <div class="col-xs-6 col-xs-offset-3" align="center"> <label for="prereadingQuestion">Pre-reading Question:</label> <select required class="form-control" id="prereadingquestion"> <option value=""></option> <option value="Based on the title, what do you think the story will be about?">Based on the title, what do you think the story will be about?</option> <option value="What do you already know about the topic?">What do you already know about the topic?</option> <option value="Based on the front cover, what do you think the story will be about?">Based on the front cover, what do you think the story will be about?</option> </select> <span>Select one of the pre-reading questions</span> </div> </div>');
@@ -114,9 +114,9 @@ jQuery(function($) {
     $(document).on ('click', '#duringreading', function() {
         
 		//Used to generate the id for the question, its answer, and the delete button
-        var question = "duringreadingQuestion" + questionCount;
-        var newAnswer = "answer" + questionCount;
-		var deletequestion = "deletQuestion" + questionCount;
+        var question = "duringreadingQuestion " + questionCount;
+        var newAnswer = "answer " + questionCount;
+		var deletequestion = "deletQuestion " + questionCount;
         
 		//Proceed with displaying the options in the during eading question and an input box for its answer, and the delete question button
         var duringreadingQuestion = $('<br><br> <div class="form-group addQuestion"> <div class="col-xs-6 col-xs-offset-3" align="center"> <label for="whilereadingQuestion">During Reading Question:</label> <select required class="form-control" id="whilereadingquestion"> <option value=""></option> <option value="What do you think will happen next based on what just happened?">What do you think will happen next based on what just happened?</option> <option value="What has happened so far?">What has happened so far?</option> <option value="What is your reaction to what just happened? How do you feel about what just happened?">What is your reaction to what just happened? How do you feel about what just happened?</option><option value="If you were this character, would you react to this situation the same way?  Would you react differently?  Why or why not?">If you were this character, would you react to this situation the same way?  Would you react differently?  Why or why not?</option> </select> <span>Select one of the while-reading questions</span> </div> </div>');
@@ -140,9 +140,9 @@ jQuery(function($) {
     $(document).on ('click', '#postreading', function() {
         
 		//Used to generate the id for the question, its answer, and the delete button
-        var question = "postreadingQuestion" + questionCount;
-        var newAnswer = "answer" + questionCount;
-		var deletequestion = "deletQuestion" + questionCount;
+        var question = "postreadingQuestion " + questionCount;
+        var newAnswer = "answer " + questionCount;
+		var deletequestion = "deletQuestion " + questionCount;
         
 		//Proceed with displaying the options in post-reading question and an input box for its answer, and the delete question button
         var postreadingQuestion = $('<br><br> <div class="form-group addQuestion"> <div class="col-xs-6 col-xs-offset-3" align="center"> <label for="postreadingQuestion">Post-reading Question:</label> <select required class="form-control" id="postreadingquestion"> <option value=""></option> <option value="How did the story end?">How did the story end?</option> <option value="Was this ending similar or different to the ending you expected?">Was this ending similar or different to the ending you expected?</option> <option value="Was there a moral or a lesson to this story?">Was there a moral or a lesson to this story?</option> <option value="Who was the main/most important character in the story?">Who was the main/most important character in the story?</option> <option value="What was the main character trying to do?">What was the main character trying to do?</option> <option value="What was the problem and solution?">What was the problem and solution?</option> <option value="What was the setting (where did the story take place)?">What was the setting (where did the story take place)?</option> <option value="What was your favorite part and why?">What was your favorite part and why?</option> <option value="What character traits did the characters have (kind, mean, creative, patient, messy)? How do you know?">What character traits did the characters have (kind, mean, creative, patient, messy)? How do you know?</option> <option value="Retell the most important events in your own words.">Retell the most important events in your own words.</option> <option value="What was the author purpose for writing the book?">What was the author purpose for writing the book?</option> <option value="What facts were in the book?">What facts were in the book?</option> <option value="What new facts did you learn?">What new facts did you learn?</option> <option value="What questions do you have after reading?">What questions do you have after reading?</option> <option value="What is your favorite fact and why?">What is your favorite fact and why?</option> <option value="Was the book fiction or non-fiction? How do you know?">Was the book fiction or non-fiction? How do you know?</option> <option value="Was there a problem in the book? What was it? How was it solved?">Was there a problem in the book? What was it? How was it solved?</option> <option value="Who was the main character? ">Who was the main character? </option> <option value="How would you describe the main character?">How would you describe the main character?</option> <option value=""></option> </select><span>Select one of the while-reading questions</span> </div> </div>');
@@ -166,9 +166,9 @@ jQuery(function($) {
     $(document).on ('click', '#createNewQuestion', function() {
         
 		//Used to generate the id for the question, its answer, and the delete button
-        var question = "newQuestion" + questionCount;
-        var newAnswer = "answer" + questionCount;
-        var deletequestion = "deletQuestion" + questionCount;
+        var question = "newQuestion " + questionCount;
+        var newAnswer = "answer " + questionCount;
+        var deletequestion = "deletQuestion " + questionCount;
 		
 		//Proceed with displaying input boxes for creating the new question and its answer, and the delete question button
         var newQuestion = $('<br><br> <div class="form-group addQuestion"> <label class="col-xs-4 col-xs-offset-1 col-lg-2 col-lg-offset-3 control-label" for="newQuestion">Create New Question:</label> <div class="col-xs-6 col-md-4 col-lg-3"> <input id="newquestion" name="newquestion" type="text" placeholder="Question" class="form-control input-md" required=""> </div> </div>');
@@ -191,7 +191,7 @@ jQuery(function($) {
 	//Function that is triggered when the button for deleting a question is clicked
 	$(document).on('click', '.deleteQuestionButton', function() {
 		
-		alert ("Deleting a question!");
+		alert ("Deleting a question " + (this).attr('id'));
 	});
     
 });
